20cbfa70058b2d197f5927e02bcbf7d1865319e3
Bichitco <bichitco1@gmail.com>
2026-03-13 22:51:28 +0300

    fix

--- 3822B1FI1/9_jacobi_kokkos/solovev_alexey/jacobi_kokkos.cpp
+++ 3822B1FI1/9_jacobi_kokkos/solovev_alexey/jacobi_kokkos.cpp
@@ -2,16 +2,29 @@
 #include <cmath>
 
 std::vector<float> JacobiKokkos(
-    const std::vector<float>& a,
-    const std::vector<float>& b,
-    float accuracy) {
+        const std::vector<float>& a,
+        const std::vector<float>& b,
+        float accuracy) {
 
     const int n = b.size();
 
     Kokkos::View<float*> x("x", n);
     Kokkos::View<float*> x_new("x_new", n);
-    Kokkos::View<const float*> vec_b(b.data(), n);
-    Kokkos::View<const float*> mat_a(a.data(), n * n);
+
+    Kokkos::View<float*> vec_b("b", n);
+    Kokkos::View<float*> mat_a("a", n * n);
+
+    auto host_b = Kokkos::create_mirror_view(vec_b);
+    auto host_a = Kokkos::create_mirror_view(mat_a);
+
+    for (int i = 0; i < n; ++i)
+        host_b(i) = b[i];
+
+    for (int i = 0; i < n * n; ++i)
+        host_a(i) = a[i];
+
+    Kokkos::deep_copy(vec_b, host_b);
+    Kokkos::deep_copy(mat_a, host_a);
 
     Kokkos::deep_copy(x, 0.0f);
 
@@ -21,17 +34,16 @@ std::vector<float> JacobiKokkos(
             Kokkos::RangePolicy<>(0, n),
             KOKKOS_LAMBDA(const int i) {
 
-            float sigma = 0.0f;
-            int row = i * n;
+                float sigma = 0.0f;
+                int row = i * n;
 
-            for (int j = 0; j < n; ++j) {
-                if (j != i) {
-                    sigma += mat_a(row + j) * x(j);
+                for (int j = 0; j < n; ++j) {
+                    if (j != i)
+                        sigma += mat_a(row + j) * x(j);
                 }
-            }
 
-            x_new(i) = (vec_b(i) - sigma) / mat_a(row + i);
-        }
+                x_new(i) = (vec_b(i) - sigma) / mat_a(row + i);
+            }
         );
 
         float max_diff = 0.0f;
@@ -39,24 +51,25 @@ std::vector<float> JacobiKokkos(
         Kokkos::parallel_reduce(
             Kokkos::RangePolicy<>(0, n),
             KOKKOS_LAMBDA(const int i, float& local_max) {
-            float diff = fabsf(x_new(i) - x(i));
-            if (diff > local_max) local_max = diff;
-        },
+                float diff = fabsf(x_new(i) - x(i));
+                if (diff > local_max) local_max = diff;
+            },
             Kokkos::Max<float>(max_diff)
         );
 
-        if (max_diff < accuracy) break;
+        if (max_diff < accuracy)
+            break;
 
         Kokkos::deep_copy(x, x_new);
     }
 
+    auto host_x = Kokkos::create_mirror_view(x);
+    Kokkos::deep_copy(host_x, x);
+
     std::vector<float> result(n);
 
-    auto host_view = Kokkos::create_mirror_view(x);
-    Kokkos::deep_copy(host_view, x);
-
     for (int i = 0; i < n; ++i)
-        result[i] = host_view(i);
+        result[i] = host_x(i);
 
     return result;
 }
